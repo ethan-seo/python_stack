afecc11bd202330fb7edb9392a0f74d11ce7b843
Ethan Seo <sung.min.seo.428@gmail.com>
2020-09-02 22:54:05 -0700

    Initializing python_stack

---
+++ hello_world.py
@@ -0,0 +1,4 @@
+x = "Hello World!"
+print(x)
+y = 42
+print(y)
---
+++ my_environments/py3Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ethan seo\onedrive\documents\cd\python_stack\my_environments\py3env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
