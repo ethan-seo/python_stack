e825d0da23728868dcac94568487389ef812f310
Ethan Seo <sung.min.seo.428@gmail.com>
2020-09-06 12:59:13 -0700

    Adding functions_intermediate2.py

--- Assignments/functions_intermediate2.py
+++ Assignments/functions_intermediate2.py
@@ -1,4 +1,4 @@
-# Update Values in Dictionaries and Lists
+# 1. Update Values in Dictionaries and Lists
 x = [ [5,2,3], [10,8,9] ] 
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -9,6 +9,7 @@ sports_directory = {
     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
 }
 z = [ {'x': 10, 'y': 20} ]
+
 # Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 x[1][0] = 15
 print(x)
@@ -18,6 +19,91 @@ students[0]["last_name"] = "Bryant"
 print(students)
 
 # In the sports_directory, change 'Messi' to 'Andres'
-# Change the value 20 in z to 30
 sports_directory["soccer"][0] = "Andres"
 print(sports_directory)
+
+# Change the value 20 in z to 30
+z[0]["y"]= 30
+print(z)
+
+# 2. Iterate Through a List of Dictionaries
+# Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+
+def iterateDictionary(list):
+    for student in list:
+        print(f"first_name - " + student["first_name"] + ", last_name - " + student["last_name"])
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+# 3. Get Values From a List of Dictionaries
+# Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary. For example, iterateDictionary2('first_name', students) should output:
+
+# Michael
+# John
+# Mark
+# KB
+# And iterateDictionary2('last_name', students) should output:
+
+# Jordan
+# Rosales
+# Guillen
+# Tonel
+
+def iterateDictionary2(key_name, list):
+    for student in list:
+        print(student[key_name])
+iterateDictionary2('last_name', students) 
+
+
+# 4. Iterate Through a Dictionary with List Values
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list. For example:
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+# printInfo(dojo)
+
+def printInfo(dict):
+    locations = dict["locations"]
+    instructors = dict["instructors"]
+    print(len(locations),"LOCATIONS")
+    for i in locations:
+        print(i)
+    print("")
+    print(len(instructors), "INSTRUCTORS")
+    for i in instructors:
+        print(i)
+printInfo(dojo) 
+
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
